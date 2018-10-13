e1032c4e033378aaf993dfc8f3f574fedb9063cb
“Brandon <broschb@gmail.com>
2018-10-12 23:54:01 -0600

    removed dead code

--- src/lib/site_builder/site/GridDragSource.jsx
+++ src/lib/site_builder/site/GridDragSource.jsx
@@ -23,66 +23,7 @@ class GridDragSource extends React.Component {
   
   constructor(props) {
     super(props);
-    // this.handleClick = this.handleClick.bind(this);
   }
-
-  // mouseEnter = (event, func, hoveredIds) => {
-  //   event.stopPropagation();
-  //   if (this.computedSelected() == false) {
-  //     func(this.props.sourceId);
-  //   }
-    
-  // }
-  // mouseLeave = (event, func, hoveredIds) => {
-  //   event.stopPropagation();
-  //   if (this.computedSelected() == false) {
-  //     func(this.props.sourceId);
-  //   }
-  // }
-
-  // handleClick(event){
-  //   event.stopPropagation();
-  //   if(this.props.selectContent != null){
-  //     this.props.selectContent({ id: this.props.sourceId });
-  //   }
-  // }
-
-  // computedSelected(){
-  //   return this.props.activeState != null && (this.props.activeState.id == this.props.sourceId);
-  // }
-
-  // computeHovered(hoveredIds){
-  //   var hov =  hoveredIds.indexOf(this.props.sourceId) > -1
-  //   return hov;
-  // }
-
-  // DragHandleRender(addId, removeId, hoveredIds, sourceId){
-  //   var hovered = this.computeHovered(hoveredIds);
-  //   var selected = this.computedSelected();
-  
-  //   if(hoveredIds.length > 0){
-  //     var longest = hoveredIds.reduce(function (a, b) { return a.split(".").length > b.split(".").length ? a : b; });
-  //     if (longest != null && longest == this.props.sourceId) {
-  //       hovered = true;
-  //     } else {
-  //       hovered = false;
-  //     } 
-  //   }
-
-  //   const { connectDragSource, isDragging } = this.props;
-  //   var handle = (this.props.handle == false || isDragging) ? false : true;
-  //   var cls = hovered ? 'draggable' : '';
-  //   cls = selected ? 'selected' : cls;
-  //   return(
-  //     <div onClick={(event) =>this.handleClick(event)} className={cls} onMouseOver={(event) => this.mouseEnter(event, addId, hoveredIds)} onMouseOut={(event) => this.mouseLeave(event, removeId, hoveredIds)}>
-  //       {(hovered && !selected) && (<div className='drag-wrapper'> {connectDragSource(
-  //         <span className="grippy"></span>
-  //       )} {this.props.children} </div>)}
-  //       {(!hovered || selected) && (this.props.children)}
-  //     </div>
-  //   )
-
-  // }
 
   DragSourceConditional(){
     const { connectDragSource, isDragging } = this.props;
@@ -97,7 +38,6 @@ class GridDragSource extends React.Component {
     return <SharedSiteConsumer key={this.props.sourceId}>
       {({ addId, removeId, hoveredIds }) => (
         <GridDragSourceRenderer addId={addId} removeId={removeId} hoveredIds={hoveredIds} {...this.props}/>
-        // this.DragHandleRender(addId, removeId, hoveredIds, this.props.sourceId)
       )}
     </SharedSiteConsumer>
   }
